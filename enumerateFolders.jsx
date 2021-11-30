03522f6a16388c4707da0c18bae71d022930fd0e
Luk-kar <59205324+Luk-kar@users.noreply.github.com>
2021-11-30 14:38:08 +0100

    update

--- enumerateFolders.jsx
+++ enumerateFolders.jsx
@@ -1,6 +1,9 @@
 #include "./helpers/isActiveDocument.jsx"
 
+enumerateElements()
+
 function enumerateElements() {
+
     if(!isActiveDocument()) {
         alert("There is no active document!");
         return; // abort program
@@ -10,6 +13,7 @@ function enumerateElements() {
 
     var buttonFolders = menu.add("button", [0,80,290,101], "folders");
     var buttonLayers = menu.add("button", [0,80,290,101], "layers");
+    var buttonClose = menu.add("button", [0,80,290,101], "close");
 
     buttonFolders.onClick = function() {
         menu.close();
@@ -21,18 +25,25 @@ function enumerateElements() {
         enumerateLayers()
     }
 
-    
+    buttonClose.onClick = function() {
+        menu.close();
+    }
+
+    menu.show();
 }
 
 function enumerateFolders() {
 
     if(confirm("Do you want to choose folder to rename it's folders?")) {
 
-        var folder = prompt("Name folder within you want to enumerate folders")
+        var folder = getFolder("layers")
+        if (folder) {
+            alert("It works!")
+        }
 
     } else {
 
-        
+
     }
 
 }
@@ -40,11 +51,45 @@ function enumerateFolders() {
 function enumerateLayers() {
     if(confirm("Do you want to choose folder to rename it's layers?")) {
 
-        var folder = prompt("Name folder within you want to enumerate folders")
+        var folder = getFolder("layers")
+        if (folder) {
+            alert("It works!")
+        }
 
     } else {
 
 
     }
 }
+
+function notifyClientAboutNoFolder(folder) {
+    alert('There is no "' + folder + '" folder in the top hierachy!', "Warning", true);
+}
+
+function getFolder(elementsToRename) {
+
+    var folder = prompt("Name folder within you want to enumerate " + elementsToRename, "folder name");
+
+    if(!checkIsFolder(folder)) {
+        notifyClientAboutNoFolder(folder);
+        return "";
+    }
+
+    return folder
+}
+
+function checkIsFolder(folderName) {
+
+    var doc = app.activeDocument;
+    var layerSets = doc.layerSets;
+
+    for (var j = 0; j < layerSets.length; j++) {
+        if (layerSets[j].name === folderName) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
 
